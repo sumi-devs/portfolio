42e3cfddbfcb4d750d4a9ac1bc7bdb70039b2c60
sumi-devs <sumithra13022004@gmail.com>
2025-07-26 20:14:53 +0530

    Fixed bug: the colour change works on home page as well

--- public/contact.js
+++ public/contact.js
@@ -1,21 +1,7 @@
-const firebaseConfig = {
-
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-
-    authDomain: "portfolio-guestbook-6d34c.firebaseapp.com",
-
-    projectId: "portfolio-guestbook-6d34c",
-
-    storageBucket: "portfolio-guestbook-6d34c.firebasestorage.app",
-
-    messagingSenderId: "740923492895",
-
-    appId: "1:740923492895:web:6ff7e3b477e323b022632a"
-
-
-};
+import firebaseConfig from './firebase-config.js';
 
 firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
 
 document.addEventListener("DOMContentLoaded", () => {
